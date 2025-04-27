a054323e077b7f34f25bfbd4012694046b6c3e00
MiankeStar <3089666858@qq.com>
2025-04-27 08:36:40 +0800

    增加文本限制
    增加标题最大字数限制

--- src/process/gfcp.py
+++ src/process/gfcp.py
@@ -28,6 +28,7 @@ keyword = config["keyword"]
 pages = config["pages"]
 min_price = config["min_price"]
 max_price = config["max_price"]
+content_range = config["content_range"]
 # 创建 ChromiumPage 实例
 browser_path = config.get("browser_path", None)
 if browser_path and not os.path.exists(browser_path):
@@ -47,6 +48,7 @@ try:
     notincluded = None if not notincluded else "".join([ni.upper() for ni in notincluded]).split(",") if is_upper else notincluded.split(",")
     is_upper = bool(is_upper)
     all_check = bool(all_check)
+    content_range = 
 
     # 打开网页
     print("打开闲鱼官网。")
@@ -100,7 +102,8 @@ try:
                 nicd = (False, ) if not notincluded else (word in tempt for word in notincluded)
                 judgei = all(icd) if all_check else any(icd)
                 judgeni = all(nicd) if all_check else any(nicd)
-                if (judgei and not judgeni) and ((0 if not min_price else min_price) <= price <= (price if not max_price else max_price)):
+                tempt_range = len(tempt)
+                if (judgei and not judgeni) and ((0 if not min_price else min_price) <= price <= (price if not max_price else max_price)) and (tempt_range <= len(content_range if not tempt_range)):
                     products[stempt] = (price, shop_name)
         except (KeyError, ValueError) as e:
             print(f"处理数据时出错: {e}")
